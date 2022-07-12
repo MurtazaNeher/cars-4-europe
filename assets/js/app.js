e09f14e9106a3bb62363f279957b79476c54cb8f
MurtazaNeher <96943172+MurtazaNeher@users.noreply.github.com>
2022-07-12 18:04:01 +0530

    Products page added

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,15 +16,3 @@ function switchHeaderForm(num) {
         document.getElementById("header-tab1").classList.remove("header-tab-active");
     }
 };
-
-$('#main').pagination({
-    dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
-    pageSize: 5,
-    showPrevious: false,
-    showNext: false,
-    callback: function(data, pagination) {
-        // template method of yourself
-        var html = template(data);
-        dataContainer.html(html);
-    }
-})
