97912b7bae682907c42e3090f534b805111eb2f4
MurtazaNeher <96943172+MurtazaNeher@users.noreply.github.com>
2022-07-14 18:52:36 +0530

    14-07-2022

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,78 +1,107 @@
 function switchHeaderForm(num) {
-  if (num == 1) {
-    document.getElementById("header-form1").style.display = "block";
-    document.getElementById("header-form2").style.display = "none";
-    document.getElementById("header-tab1").classList.add("header-tab-active");
-    document
-      .getElementById("header-tab1")
-      .classList.remove("header-tab-inactive");
-    document.getElementById("header-tab2").classList.add("header-tab-inactive");
-    document
-      .getElementById("header-tab2")
-      .classList.remove("header-tab-active");
-  } else {
-    document.getElementById("header-form1").style.display = "none";
-    document.getElementById("header-form2").style.display = "block";
-    document.getElementById("header-tab2").classList.add("header-tab-active");
-    document
-      .getElementById("header-tab2")
-      .classList.remove("header-tab-inactive");
-    document.getElementById("header-tab1").classList.add("header-tab-inactive");
-    document
-      .getElementById("header-tab1")
-      .classList.remove("header-tab-active");
-  }
+    if (num == 1) {
+        document.getElementById("header-form1").style.display = "block";
+        document.getElementById("header-form2").style.display = "none";
+        document.getElementById("header-tab1").classList.add("header-tab-active");
+        document
+            .getElementById("header-tab1")
+            .classList.remove("header-tab-inactive");
+        document.getElementById("header-tab2").classList.add("header-tab-inactive");
+        document
+            .getElementById("header-tab2")
+            .classList.remove("header-tab-active");
+        document
+            .getElementById("header-tab1")
+            .classList.remove("l-tab");
+        document
+            .getElementById("header-tab2")
+            .classList.add("r-tab");
+
+    } else {
+        document.getElementById("header-form1").style.display = "none";
+        document.getElementById("header-form2").style.display = "block";
+        document.getElementById("header-tab2").classList.add("header-tab-active");
+        document
+            .getElementById("header-tab2")
+            .classList.remove("header-tab-inactive");
+        document.getElementById("header-tab1").classList.add("header-tab-inactive");
+        document
+            .getElementById("header-tab1")
+            .classList.remove("header-tab-active");
+        document
+            .getElementById("header-tab2")
+            .classList.remove("r-tab");
+        document
+            .getElementById("header-tab1")
+            .classList.add("l-tab");
+    }
 }
 
 // Initialize Swiper
 var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
 var swiper2 = new Swiper(".mySwiper2", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
 });
 
 // Show password : Registration page
 function showRcPass() {
-  var x = document.getElementById("rc-pass");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
+    var x = document.getElementById("rc-pass");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
 }
 
 //   Select List Logic
 
-function setSelectBtn() {
-    let selectForm = document.getElementById("home-company-select");
+function triggerDropdown() {
+    if (document.getElementById("index-header-d1-con").style.display === "none") {
+        document.getElementById("index-header-d1-con").style.display = "block";
+        document.getElementById("index-header-d1-con").style.borderTop = "none";
+        document.getElementById("index-header-d1-btn").style.borderBottom = "none";
+    } else {
+        document.getElementById("index-header-d1-con").style.display = "none";
+        document.getElementById("index-header-d1-btn").style.borderBottom = "1px solid #39379a4d";
+    }
+}
 
-  if (selectForm.options[selectForm.selectedIndex].value != "0") {
+function setDropdownVal(val) {
+    document.getElementById("index-header-d1-btn-p").innerText = val;
     document.getElementById("hcs-clear-btn").style.display = "block";
-  }
-}
-function clearSelect() {
-let selectForm = document.getElementById("home-company-select");
+    document.getElementById("open-dd").style.display = "none";
 
-  if (selectForm.selectedIndex != 0) {
-    selectForm.selectedIndex = 0;
+    triggerDropdown();
+
+}
+
+function clearDD() {
+    document.getElementById("index-header-d1-btn-p").innerText = "Honda";
     document.getElementById("hcs-clear-btn").style.display = "none";
-   }
-}
+    document.getElementById("open-dd").style.display = "block";
 
+
+};
+
+// Tootltip
+function showToolTip(mssg) {
+    console.log(mssg)
+}
